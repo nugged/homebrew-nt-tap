c6da7966d3b6fd9797642eac6d09d3448851bd36
Andrii Nugged <nugged@gmail.com>
2025-07-02 09:10:08 +0300

    Make it more look like original one.

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -1,28 +1,45 @@
-class Fzf < Formula
-  desc "Command-line fuzzy finder"
-  homepage "https://github.com/nugged/fzf"
-  url "https://github.com/nugged/fzf.git",
-      using:    :git,
-      branch:   "fix-nil-pointer-dereference-crash"
-  version "0.63.0-fix"
-  license "MIT"
+ class Fzf < Formula
+   desc "Command-line fuzzy finder"
+   homepage "https://github.com/nugged/fzf"
+   url "https://github.com/nugged/fzf.git",
+       using:    :git,
+       branch:   "fix-nil-pointer-dereference-crash"
+   version "0.63.0-fix"
+   license "MIT"
 
-  depends_on "go" => :build
+   depends_on "go" => :build
 
-  def install
-    # Build a single-arch binary for this machine
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+   def install
+     # Build the binary with version and revision info
+     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version} -X main.revision=brew")
 
-    # Shell completions
-    # bash_completion.install "shell/completion.bash" => "fzf.bash"
-    # zsh_completion.install  "shell/completion.zsh"    => "_fzf"
-    # fish_completion.install "shell/completion.fish"
+     # Install binaries and helper scripts
+     man1.install "man/man1/fzf.1",    "man/man1/fzf-tmux.1"
+     bin.install "bin/fzf-tmux"
+     bin.install "bin/fzf-preview.sh"
 
-    # Manpage
-    man1.install "man/man1/fzf.1"
-  end
+     # Shell integration scripts (install/uninstall, key-bindings, completions)
+     prefix.install "install", "uninstall"
+     (prefix/"shell").install %w[bash zsh fish].map { |s| "shell/key-bindings.#{s}" }
+     (prefix/"shell").install %w[bash zsh].map        { |s| "shell/completion.#{s}" }
+     (prefix/"plugin").install "plugin/fzf.vim"
+   end
 
-  test do
-    assert_match "fzf", shell_output("#{bin}/fzf --version")
-  end
-end
+   def caveats
+     <<~EOS
+       To set up shell integration, see:
+         https://github.com/junegunn/fzf#setting-up-shell-integration
+       To use fzf in Vim, add the following line to your .vimrc:
+         set rtp+=#{opt_prefix}
+     EOS
+   end
+
+   test do
+     assert_match "fzf", shell_output("#{bin}/fzf --version")
+   end
+   test do
+     (testpath/"list").write %w[hello world].join($INPUT_RECORD_SEPARATOR)
+     output = pipe_output("#{bin}/fzf -f wld", (testpath/"list").read).chomp
+     assert_equal "world", output
+   end
+ end
